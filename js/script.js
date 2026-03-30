4f3c3754de3a679ee64694386fb54cdfa1bf8982
Ucokike18 <okikeuchennac@gmail.com>
2026-03-30 02:44:34 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const registerButton = document.querySelector(".register");
+const loginButton = document.querySelector(".login");
+
+registerButton.addEventListener("click", function () {
+  alert("Registration page coming soon");
+});
+
+loginButton.addEventListener("click", function () {
+  alert("Login page coming soon");
+});
